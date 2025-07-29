10f00174b11051492a8fb36ad87a9dbf0e051554
Kanniget <inicholls@gmail.com>
2025-07-29 21:55:21 +1000

    Use server icon for host

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -4,7 +4,7 @@ import * as d3 from 'd3';
 
 const icons = {
     net: '/icons/cloud.svg',
-    host: '/icons/computer-desktop.svg',
+    host: '/icons/server-stack.svg',
     vm: '/icons/computer-desktop.svg',
     rtr: '/icons/server-stack.svg',
     fw: '/icons/shield-check.svg',
